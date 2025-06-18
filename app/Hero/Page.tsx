83b801ea8473bba8e7b8fe9d757d4635e7ab0b0d
maryammuhammadafzal <natiqnagori7@gmail.com>
2025-06-18 17:19:48 +0500

    creating hero

--- app/Hero/Page.tsx
+++ app/Hero/Page.tsx
@@ -1,3 +1,4 @@
+import Header from "@/components/Header";
 import React from "react";
 // import HeroBgVideo from '@/public/video/Hero-bg-video.mp4'
 const HeroPage = () => {
@@ -7,11 +8,14 @@ const HeroPage = () => {
         <source src="/video/Hero-bg-video.mp4" type="video/mp4" />
       </video>
       <div className="overlay w-full h-full bg-black/20 absolute top-0 rounded-[10px] left-0 z-10"></div>
-      <div className="w-full h-full absolute top-0 left-0 flex ">
-        <div className="lg:w-[90%] h-auto flex flex-col justify-between w-full lg:px-0 px-3 py-5">
-                <div className="header w-full h-[100px] flex justify-center items-center">
-                        
-                </div>
+      <div className="w-full h-full absolute top-0 left-0 flex justify-center">
+        <div className="lg:w-[90%] h-auto flex flex-col justify-between items-center w-full lg:px-0 px-3 py-5">
+          <div className="header bg-amber-50 w-full h-[100px] flex justify-center items-center">
+                <Header/>
+          </div>
+          <div className="max-w-4xl">
+                <h1 className="text-8xl text-white uppercase font-bold">Unleashing Potential at Little Minds Academy</h1>
+          </div>
         </div>
       </div>
     </div>
