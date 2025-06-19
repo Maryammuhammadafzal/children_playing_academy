af340753827b3daa052527f30e797bd770720ca0
maryammuhammadafzal <natiqnagori7@gmail.com>
2025-06-19 22:11:34 +0500

    created faetures section

--- app/Features/Page.tsx
+++ app/Features/Page.tsx
@@ -21,35 +21,35 @@ const FeaturesPage = () => {
     },
   ]
   return (
-    <div className="w-full h-auto flex justify-center items-center p-9 relative">
-      <div className="vector absolute left-0 top-0 w-[200px] h-[300px]">
+    <div className="w-full h-auto flex justify-center items-center py-20">
+      <div className="vector absolute -left-5 -top-40 w-auto h-auto z-50">
         <Image
           src="/images/vector-1.png"
           alt="vector"
-          width={200}
-          height={1000}
-          className="w-[200px]"
+          width={100}
+          height={100}
+          className="w-[100px]"
         />
       </div>
-      <div className="w-full h-auto flex flex-col gap-6 justify-center items-center">
+      <div className="w-full h-auto flex flex-col gap-14 justify-center items-center">
           <h2 className="text-primary font-bold xl:text-6xl sm:text-4xl text-3xl lg:text-5xl md:text-4xl font-sans ">
               Key Features
             </h2>
 
-            <div className="flex justify-center items-center flex-wrap gap-14 w-full h-auto">
+            <div className="flex justify-center items-center flex-wrap gap-0 xl:gap-14 w-full h-auto">
                {features_data && features_data.map((feature , index)=>(
-                <div data-aos='zoom-in'
-                      className="sm:w-[500px] w-full h-[260px] flex justify-center items-center bg-cover bg-center relative"
+                <div key={index} data-aos='zoom-in'
+                      className="lg:w-[500px] w-[450px] md:w-[50%] md:h-auto md:my-43xs:my-2 my-1 lg:h-[300px] flex justify-center bg-cover bg-center relative"
                       // style={{ backgroundImage: `url('/images/texture.png')` }}
                     >
-                      <Image src='/images/texture.png' alt="texture" width={500} height={250} />
+                      <Image src='/images/texture.png' alt="texture" width={500} height={300} />
 
-                      <div className="absolute w-full h-full flex justify-center items-center top-0  left-0">
-                        <div className="w-full h-full flex flex-col justify-center py-10 px-9 gap-4">
-                          <h2 className="text-white font-bold xl:text-3xl sm:text-xl text-lg lg:text-2xl md:text-4xl font-sans ">
+                      <div className="absolute w-full h-full flex justify-center  top-0  left-0">
+                        <div className="w-full h-full flex flex-col py-10 px-10 gap-3">
+                          <h2 className="text-white font-mono font-bold lg:text-3xl md:text-2xl text-3xl  ">
                             {feature.title}
                           </h2>
-                          <p className="text-white font-mono xl:text-2xl sm:text-xl text-base lg:text-lg ">
+                          <p className="text-white font-mono lg:text-xl text-lg ">
                             {feature.description}
                           </p>
                         </div>
