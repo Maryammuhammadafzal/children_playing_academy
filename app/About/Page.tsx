6c5288a17ade731da554785837bd333c120a3305
maryammuhammadafzal <natiqnagori7@gmail.com>
2025-06-19 15:37:16 +0500

    making responsive

--- app/About/Page.tsx
+++ app/About/Page.tsx
@@ -3,19 +3,30 @@ import React from "react";
 
 const AboutPage = () => {
   return (
-    <div className="w-full h-auto flex py-10 justify-center items-center gap-3">
-      <div className="bg-primary h-[400px] relative w-[750px] rounded-[10px] p-10 flex justify-center ">
+    <div className="w-full h-auto flex py-10 justify-center items-center gap-12">
+      <div className="bg-primary h-[400px] relative w-[760px] rounded-[10px] p-10 flex justify-center ">
         <p className="text-[32px] leading-10 text-white font-mono ">
-                At Little Minds Academy, we believe that every child is unique and
-        deserves a nurturing environment to learn, grow, and play. Our platform
-        combines education and play to create a holistic development experience
-        for children, fostering creativity, curiosity, and confidence.
+          At Little Minds Academy, we believe that every child is unique and
+          deserves a nurturing environment to learn, grow, and play. Our
+          platform combines education and play to create a holistic development
+          experience for children, fostering creativity, curiosity, and
+          confidence.
         </p>
-        <div className="absolute bottom-0 right-0">
-                <Image 
-                
-                />
+        <div className="absolute -bottom-20 -right-70">
+          <Image
+            src="/images/bat-image.png"
+            alt="bat-image"
+            width={650}
+            height={300}
+            className="-rotate-[100deg]"
+          />
         </div>
+      </div>
+      <div
+        className="w-[610px] h-[400px] flex justify-center items-center bg-cover bg-center relative"
+        style={{ backgroundImage: `url('/images/texture.png')` }}
+      >
+        <div className="absolute w-full h-full flex justify-center items-center top-0 left-0"></div>
       </div>
     </div>
   );
