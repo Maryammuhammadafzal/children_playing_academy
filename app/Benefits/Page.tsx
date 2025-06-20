88bffbee52f06faf79e734d678ffeb5c2e02c060
maryammuhammadafzal <natiqnagori7@gmail.com>
2025-06-20 10:37:21 +0500

    created benefits section responsive

--- app/Benefits/Page.tsx
+++ app/Benefits/Page.tsx
@@ -2,37 +2,42 @@ import Image from "next/image";
 import React from "react";
 
 const BenefitsPage = () => {
-  let features_data = [
+  let benefits_data = [
     {
+      icon:'/images/benefit-icon1',
       title: "Enhanced Cognitive Skills",
       description: "Problem-solving, logical reasoning, and critical thinking.",
     },
     {
+      icon:'/images/benefit-icon2',
       title: "Improved Social Skills",
       description: "Teamwork, communication, and empathy.",
     },
     {
+      icon:'/images/benefit-icon3',
       title: "Boosted Creativity",
       description: "Artistic expression and innovative thinking.",
     },
     {
+      icon:'/images/benefit-icon4',
       title: "Physical Development",
       description: "Activities that promote coordination and overall health.",
     },
     {
+      icon:'/images/benefit-icon5',
       title: "Confidence Building",
       description: "Celebrating achievements and encouraging self-expression.",
     },
   ];
   return (
     <div className="w-full h-auto flex justify-center items-center py-20">
-      <div className="vector absolute -left-5 -top-40 w-auto h-auto z-50">
+      <div className="vector absolute -right-5 -top-40 w-auto h-auto z-50">
         <Image
           src="/images/vector-1.png"
           alt="vector"
-          width={100}
+          width={200}
           height={100}
-          className="w-[100px]"
+          className="w-[200px]"
         />
       </div>
       <div className="w-full h-auto flex flex-col gap-14 justify-center items-center">
@@ -40,31 +45,28 @@ const BenefitsPage = () => {
           Benefits for Your Child
         </h2>
 
-        <div className="flex justify-center items-center flex-wrap gap-0 xl:gap-14 w-full h-auto">
-          {features_data &&
-            features_data.map((feature, index) => (
+        <div className="flex justify-center items-center flex-wrap gap-16 w-full h-auto">
+          {benefits_data &&
+            benefits_data.map((benefit, index) => (
               <div
                 key={index}
                 data-aos="zoom-in"
-                className="lg:w-[500px] w-[450px] md:w-[50%] md:h-auto md:my-43xs:my-2 my-1 lg:h-[300px] flex justify-center bg-cover bg-center relative"
-                // style={{ backgroundImage: `url('/images/texture.png')` }}
+                className="w-[340px] h-auto min-h-[190px] px-3 py-2 flex justify-center items-center"
               >
                 <Image
-                  src="/images/texture.png"
-                  alt="texture"
-                  width={500}
-                  height={300}
+                  src={benefit.icon}
+                  alt="icon"
+                  width={70}
+                  height={70}
                 />
 
-                <div className="absolute w-full h-full flex justify-center  top-0  left-0">
-                  <div className="w-full h-full flex flex-col py-10 px-10 gap-3">
-                    <h2 className="text-white font-mono font-bold lg:text-3xl md:text-2xl text-3xl  ">
-                      {feature.title}
+                <div className="w-full h-full flex justify-center text-center flex-col gap-3 items-center">
+                    <h2 className="text-primary font-mono font-bold text-2xl ">
+                      {benefit.title}
                     </h2>
-                    <p className="text-white font-mono lg:text-xl text-lg ">
-                      {feature.description}
+                    <p className="text-[#969696] font-mono text-lg ">
+                      {benefit.description}
                     </p>
-                  </div>
                 </div>
               </div>
             ))}
